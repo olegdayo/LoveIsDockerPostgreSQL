46ef7fb6aab6f8892306aca6a94aa1ed183be63e
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2021-06-11 16:12:38 +0300

    Add method to get country info by name

--- LoveIsDockerPostgreSQL/Controllers/CountryController.cs
+++ LoveIsDockerPostgreSQL/Controllers/CountryController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Dapper;
+using LoveIsDockerPostgreSQL.Models;
 using Npgsql;
 
 namespace LoveIsDockerPostgreSQL.Controllers
@@ -16,18 +18,30 @@ namespace LoveIsDockerPostgreSQL.Controllers
     {
         private const string ConnectionString =
             "Host=localhost;Port=5432;User Id=postgres;Password=postgres;Database=postgres;";
-
-        public CountryController()
-        {
-            
-        }
         
+        /// <summary>
+        /// Returns the list of countries in the database
+        /// </summary>
+        /// <returns>List of countries' names</returns>
         [HttpGet("get_countries")]
         public async Task<IActionResult> GetCountries()
         {
             await using var dbConnection = new NpgsqlConnection(ConnectionString);
             dbConnection.Open();
             return new OkObjectResult(dbConnection.Query<string>("SELECT distinct name from oof.countries;"));
+        }
+        
+        /// <summary>
+        /// Returns info about the specific country by its name
+        /// </summary>
+        /// <param name="countryName">A name of the country</param>
+        /// <returns></returns>
+        [HttpGet("get_country_info_by_name")]
+        public async Task<IActionResult> GetCountryInfoByName([Required] string countryName)
+        {
+            await using var dbConnection = new NpgsqlConnection(ConnectionString);
+            dbConnection.Open();
+            return new OkObjectResult(dbConnection.Query<CountryInfo>("SELECT *  from oof.countries where name = @countryName;", new {countryName}));
         }
     }
 }
---
+++ LoveIsDockerPostgreSQL/Models/CountryInfo.cs
@@ -0,0 +1,11 @@
+namespace LoveIsDockerPostgreSQL.Models
+{
+    public class CountryInfo
+    {
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string Subregion { get; set; }
+        public string Capital { get; set; }
+        public int Id { get; set; }
+    }
+}
